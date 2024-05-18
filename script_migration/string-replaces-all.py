6babf33abc86bd6959749dcd12763a41568cc372
gauravkhanna3007 <gaurav3007@gmail.com>
2024-05-18 13:19:10 +0530

    Testing

--- script_migration/string-replaces-all.py
+++ script_migration/string-replaces-all.py
@@ -12,7 +12,7 @@ def replace_strings(excel_path, repo_path):
     exclude_dirs = [os.path.join(repo_path, 'input_migration'), os.path.join(repo_path, '.git'), os.path.join(repo_path, 'script_migration')]
 
     # Get list of all files in repository (excluding .git directory)
-    all_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(repo_path) for f in filenames if not any(os.path.abspath(dp) == os.path.abspath(exclude_dir) for exclude_dir in exclude_dirs)]
+    all_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(repo_path) for f in filenames if not any(os.path.abspath(dp).startswith(os.path.abspath(exclude_dir) + os.sep) for exclude_dir in exclude_dirs)]
 
     print(f"All files: {all_files}")
 
