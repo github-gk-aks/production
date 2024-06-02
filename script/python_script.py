f25e8b4325ced77a205f3efd890351b5aeba37a7
gauravkhanna3007 <38395482+gauravkhanna3007@users.noreply.github.com>
2024-06-02 21:58:34 +0530

    Update python_script.py

--- script/python_script.py
+++ script/python_script.py
@@ -1,1 +1,14 @@
-test
+import sys
+import pandas as pd
+
+repository_name = sys.argv[1]  # Repository name passed as a command line argument
+
+# Read Excel file
+excel_data = pd.read_excel('data/Repository-List.xlsx', header=None)
+repository_names = excel_data.iloc[:, 0].tolist()
+
+# Check if the current repository name is in the list
+allow_access = repository_name in repository_names
+
+# Output 'true' or 'false' to be used in the subsequent step
+print(f"::set-output name=allowAccess::{str(allow_access).lower()}")
